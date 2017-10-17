5b1f2db5c2643b0b4d16614c1c98573212c8fb96
Patrick Sherwood <patrick.sherwood@cirque.com>
2017-10-17 09:39:53 -0600

    Added project files.

---
+++ Gen4_I2C_Demo/I2C.cpp
@@ -0,0 +1,41 @@
+#include <stdint.h>
+#include <stdbool.h>
+#include <Wire.h>
+#include "I2C.h"
+
+
+void I2C_init(uint32_t clockFrequency)
+{
+  Wire.setClock(clockFrequency);
+  Wire.begin();
+}
+
+void I2C_request(uint16_t address, uint16_t count, uint16_t stop)
+{
+  Wire.requestFrom(address, count, stop);
+}
+
+uint16_t I2C_available()
+{
+  return Wire.available();
+}
+
+uint8_t I2C_read()
+{
+  return Wire.read();
+}
+
+void I2C_write(uint8_t data)
+{
+  Wire.write(data);
+}
+
+void I2C_beginTransmission(uint8_t address)
+{
+  Wire.beginTransmission(address);
+}
+
+void I2C_endTransmission(bool stop)
+{
+  Wire.endTransmission(stop);
+}
